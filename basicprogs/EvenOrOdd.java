0f445266973d8c321eb929dc67b8986b165fc50f
sahilburle <sahilburle1998@gmail.com>
2022-07-17 15:35:55 +0530

    odd even

--- basicprogs/EvenOrOdd.java
+++ basicprogs/EvenOrOdd.java
@@ -7,5 +7,20 @@ public class EvenOrOdd {
     public static void main(String[] args) {
 
         Scanner num = new Scanner(System.in);
+
+        System.out.println("Enter ur number");
+
+        int no = num.nextInt();
+
+        if (no == 0){
+            System.out.println("number is zero");
+        }
+        else if (no % 2 == 0){
+            System.out.println("num is even");
+        }
+        else
+        {
+            System.out.println("num is odd");
+        }
     }
 }
